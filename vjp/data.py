51d618dd7b38220e964b72a2bcfd63b5b88d0b19
Francesco <francesco.mistri98@gmail.com>
2023-02-01 15:14:08 +0100

    Add load directory function
    Load all instances in a list.

--- vjp/data.py
+++ vjp/data.py
@@ -1,5 +1,6 @@
 """Data loading and manipulation."""
 import os
+from typing import List
 import xml.etree.ElementTree as ET
 
 import importlib_resources as resources
@@ -8,3 +9,16 @@ import importlib_resources as resources
 def load_instance_raw(file: os.PathLike) -> ET.Element:
     """Load and return an XML instance tree, with no cleanup."""
     return ET.parse(file).getroot()
+
+
+def load_directory(directory: resources.abc.Traversable) -> List[ET.Element]:
+    """Load all instances from a given directory in a list.
+
+    Does not explore subdirectories.
+    """
+    instances = []
+    for file in directory.iterdir():
+        if file.is_file():
+            instances.append(load_instance_raw(file))
+
+    return instances
