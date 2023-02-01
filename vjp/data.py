e625299dd57fae73b051663b84f6fc0833ef9f56
Francesco <francesco.mistri98@gmail.com>
2023-02-01 19:37:24 +0100

    Add load_second_instance function
    Load second instance samples from the default included dataset.

--- vjp/data.py
+++ vjp/data.py
@@ -5,6 +5,12 @@ import xml.etree.ElementTree as ET
 
 import importlib_resources as resources
 
+OTHER_OUTCOMES_RESOURCES = 'vjp.dataset.OtherOutcomes'
+SECOND_INSTANCE_REJECT_RESOURCES = 'vjp.dataset.Reject.SecondInstance'
+FIRST_INSTANCE_REJECT_RESOURCES = 'vjp.dataset.Reject.FirstInstance'
+SECOND_INSTANCE_UPHOLD_RESOURCES = 'vjp.dataset.Uphold.SecondInstance'
+FIRST_INSTANCE_UPHOLD_RESOURCES = 'vjp.dataset.Uphold.FirstInstance'
+
 
 def load_instance_raw(file: os.PathLike) -> ET.Element:
     """Load and return an XML instance tree, with no cleanup."""
@@ -22,3 +28,11 @@ def load_directory(directory: resources.abc.Traversable) -> List[ET.Element]:
             instances.append(load_instance_raw(file))
 
     return instances
+
+
+def load_second_instance() -> List[ET.Element]:
+    """Load all second instance samples from the default dataset."""
+    return (
+        load_directory(resources.files(SECOND_INSTANCE_UPHOLD_RESOURCES))
+        + load_directory(resources.files(SECOND_INSTANCE_REJECT_RESOURCES))
+    )
